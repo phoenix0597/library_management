ee46ca00bba4682e001bb7986fd28f6f41d86df9
Stanislav M <phoenix0597@yandex.ru>
2024-11-30 02:03:59 +0300

    Add docstrings to storage.py module

--- library_app/storage.py
+++ library_app/storage.py
@@ -1,18 +1,49 @@
 import json
 import os
 
+from library_app.book import Book
+
 
 class Storage:
-    def __init__(self, filename):
+    """
+    Класс для работы с хранилищем книг в формате JSON.
+
+    Attributes:
+        filename (str): Имя файла, в котором хранятся данные о книгах.
+    """
+
+    def __init__(self, filename: str):
+        """
+        Инициализация объекта Storage.
+
+        Args:
+            filename (str): Имя файла для хранения данных о книгах.
+        """
         self.filename = filename
 
-    def save_books(self, books: list):
+    def save_books(self, books: list[Book]) -> None:
+        """
+        Сохраняет список книг в файл.
+
+        Args:
+            books (List[Any]): Список объектов книг, которые будут сохранены.
+
+        Returns:
+            None
+        """
         data = [book.__dict__ for book in books]
         # print("Данные для сохранения:", data)
         with open(self.filename, "w", encoding="utf-8") as lib_file:
             json.dump(data, lib_file, indent=4, ensure_ascii=False)
 
-    def load_books(self):
+    def load_books(self) -> list[Book]:
+        """
+        Загружает список книг из файла.
+
+        Returns:
+            List[Book]: Список словарей с данными о книгах.
+        """
+
         if os.path.exists(self.filename):
             with open(self.filename, "r", encoding="utf-8") as file:
                 data = json.load(file)
@@ -21,7 +52,17 @@ class Storage:
         return []
 
     # обновление данных конкретной книги в файле БД
-    def update_book(self, book_id, updated_book_data):
+    def update_book(self, book_id: int, updated_book_data: dict[str, any]) -> None:
+        """
+        Обновляет данные конкретной книги в файле.
+
+        Args:
+            book_id (int): ID книги, которую нужно обновить.
+            updated_book_data (dict[str, Any]): Новые данные для книги.
+
+        Returns:
+            None
+        """
         books_data = self.load_books()
         for i, book_data in enumerate(books_data):
             if book_data["id"] == book_id:
