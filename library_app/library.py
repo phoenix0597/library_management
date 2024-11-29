e2177e19988541aa38e2fc5be67f30186477218e
Stanislav M <phoenix0597@yandex.ru>
2024-11-30 02:52:44 +0300

    Add docstrings in library.py

--- library_app/library.py
+++ library_app/library.py
@@ -1,17 +1,33 @@
-from library_app.book import Book
-from library_app.config import DATABASE_FILE_PATH
-from library_app.storage import Storage
+from book import Book
+from config import DATABASE_FILE_PATH
+from storage import Storage
 
 
 class Library:
+    """
+    Класс, представляющий библиотеку книг.
+
+    Attributes:
+        books (list[Book]): Список книг в библиотеке.
+        storage (Storage): Объект для работы с хранилищем данных.
+    """
+
     def __init__(self):
-        self.books = []
-        self.storage = Storage(DATABASE_FILE_PATH)
+        """
+        Инициализирует объект библиотеки.
+
+        Загружает книги из хранилища при инициализации.
+        """
+        self.books: list[Book] = []
+        self.storage: Storage = Storage(DATABASE_FILE_PATH)
         # Загрузим книги из БД при инициализации библиотеки
         self.load_books()
 
     # Добавляем метод для загрузки книг из файла
-    def load_books(self):
+    def load_books(self) -> None:
+        """
+        Загружает книги из хранилища и сохраняет их в список books.
+        """
         books_data = self.storage.load_books()
         self.books = [
             Book(
@@ -24,14 +40,38 @@ class Library:
             for book_data in books_data
         ]
 
-    def add_book(self, title, author, year):
+    def add_book(self, title: str, author: str, year: int) -> Book:
+        """
+        Добавляет новую книгу в библиотеку.
+
+        Аргументы:
+            title (str): Название книги.
+            author (str): Автор книги.
+            year (int): Год издания книги.
+
+        Returns:
+            Book: Объект добавленной книги.
+        """
         id_ = max([book.id for book in self.books], default=0) + 1
         book = Book(id_, title, author, year)
         self.books.append(book)
         self.storage.save_books(self.books)
+        print(f"Книга добавлена: {book}")
         return book
 
-    def remove_book(self, id_):
+    def remove_book(self, id_: int) -> bool:
+        """
+        Удаляет книгу из библиотеки по её идентификатору.
+
+        Args:
+            id_ (int): Идентификатор книги.
+
+        Returns:
+            bool: True, если книга была успешно удалена, иначе вызывает ValueError.
+
+        Вызывает:
+            ValueError: Если книга с указанным идентификатором не найдена.
+        """
         for book in self.books:
             if book.id == id_:
                 self.books.remove(book)
@@ -40,7 +80,16 @@ class Library:
                 return True
         raise ValueError("Книга с таким id не найдена!")
 
-    def find_books(self, query: str):
+    def find_books(self, query: str) -> list[Book]:
+        """
+        Ищет книги по заданному запросу.
+
+        Args:
+            query (str): Строка запроса для поиска по названию, автору или году издания.
+
+        Returns:
+            list[Book]: Список книг, соответствующих запросу.
+        """
         query = query.lower()
         return [
             book
@@ -50,17 +99,39 @@ class Library:
             or query in str(book.year)
         ]
 
-    def find_book_by_id(self, id_):
+    def find_book_by_id(self, id_: int) -> Book | None:
+        """
+        Ищет книгу по её идентификатору.
+
+        Args:
+            id_ (int): Идентификатор книги.
+
+        Returns:
+            Optional[Book]: Объект книги, если найден, иначе None.
+        """
         for book in self.books:
             if book.id == id_:
                 return book
 
-    def display_books(self):
+    def display_books(self) -> None:
+        """
+        Выводит информацию о всех книгах в библиотеке.
+        """
         for book in self.books:
             print(book)
 
     # для обновления статуса книги в хранилище
-    def update_book_status(self, book_id, new_status):
+    def update_book_status(self, book_id: int, new_status: str) -> bool:
+        """
+        Обновляет статус книги в хранилище.
+
+        Args:
+            book_id (int): Идентификатор книги.
+            new_status (str): Новый статус книги.
+
+        Returns:
+            bool: True, если статус был успешно обновлен, иначе False.
+        """
         book = self.find_book_by_id(book_id)
         if book:
             book.change_book_status(new_status)
